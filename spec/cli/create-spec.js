26d57856e3da881d4ee0701d29367cb01b26d1a2
James Abdul <james.abdul@craftturf.com>
2013-12-08 16:39:09 +0000

    Reorganising structure. Now includes optimist library.

---
+++ spec/cli/create-spec.js
@@ -0,0 +1,15 @@
+/**
+ * Module Dependencies
+ * @type {*}
+ */
+var cli = require('../../lib/cli'),
+    apln = require('../../lib/apln');
+
+/**
+ * Specification: $ apln create <module-name>
+ */
+describe('$ apln create <module-name>', function(){
+  it('should create the module', function() {
+
+  });
+});
