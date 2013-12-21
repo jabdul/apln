6b4ad630ac43ee1730c62c1d0034658d4eab8c39
James Abdul <james.abdul@craftturf.com>
2013-12-21 15:41:28 +0000

    Create's init test failing.

--- spec/cli/create-spec.js
+++ spec/cli/create-spec.js
@@ -9,12 +9,15 @@ var cli = require('../../lib/cli'),
  * Specification: $ apln create <module-name>
  */
 describe('$ apln create <module-name>', function(){
+  var that = this;
   beforeEach(function() {
-    spyOn(apln, 'create');
+    that.Create = new apln.create();
+    spyOn(that.Create, 'init');
   });
 
   it('should create the module', function() {
     cli.argv({_: ['create','my-app']});
-    expect(apln.create).toHaveBeenCalledWith('my-app');
+    //expect(that.Create.init).toHaveBeenCalledWith('my-app');
+    expect(that.Create.tag).toBe('creator');
   });
 });
