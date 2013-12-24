086bf2241794bcaaa6933a5315fc9dc81fcf737f
James Abdul <james.abdul@craftturf.com>
2013-12-24 15:48:26 +0000

    Added module-name validation to API.

--- spec/cli/create-spec.js
+++ spec/cli/create-spec.js
@@ -10,13 +10,25 @@ var cli = require('../../lib/cli'),
  */
 describe('$ apln create <module-name>', function(){
   var that = this;
+
+  that.moduleName = 'my-app';
+  that.prop = ['moduleName', 'nameSpace'];
   beforeEach(function() {
     spyOn(apln.create, 'init');
   });
 
-  it('should create the module', function() {
-    cli.argv({_: ['create','my-app']});
-    expect(apln.create.init).toHaveBeenCalledWith('my-app');
-    expect(apln.create.tag).toBe('creator');
+  it('should initialise the process', function() {
+    cli.argv({_: ['create',that.moduleName]});
+    expect(apln.create.init).toHaveBeenCalledWith(that.moduleName);
+    expect(apln.create.moduleName).not.toBe(that.moduleName);
+  });
+
+  it('should validate module name', function() {
+    expect(apln.create.validate(that.prop[0],that.moduleName)).toBe(true);
+    expect(apln.create.validate(that.prop[0], 'm')).toBe(true);
+    expect(apln.create.validate(that.prop[0],'9my-app')).toBe(false);
+    expect(apln.create.validate(that.prop[0],'*')).toBe(false);
+    expect(apln.create.validate(that.prop[0],'-my-app')).toBe(false);
+    expect(apln.create.validate(that.prop[0],'')).toBe(false);
   });
 });
