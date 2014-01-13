1426f149423d8e5335206deee2b6c4fd535b1891
James Abdul <james.abdul@craftturf.com>
2014-01-13 15:02:33 +0000

    Adding tests for cli controllers.

---
+++ spec/cli/argv-spec.js
@@ -0,0 +1,44 @@
+/**
+ * Module Dependencies
+ * @type {*}
+ */
+var cli = require('../../lib/cli'),
+    apln = require('../../lib/apln'),
+    path = require('path');
+
+/**
+ * Specification: $ apln <command> <value> <options>
+ */
+describe('$ apln <command> <value> <options>', function(){
+  var that = this;
+
+  that.moduleName = 'my-app';
+  that.nameSpace = 'my-app-ns';
+  that.commands = ['create', 'c', 'remove', 'r'];
+  that.options = ['verbose', 'quiet', 'help'];
+  that.appland = path.resolve(__dirname, '../fixture/appland');
+  that.argv = {
+    _: [ that.commands[0], that.moduleName ],
+    '$0': that.appland,
+    silent: undefined,
+    verbose: true,
+    help: undefined
+  };
+
+  beforeEach(function() {
+    spyOn(cli, 'argv');
+    cli.argv(that.argv);
+  });
+
+  it('tracks the calling of argv spy', function() {
+    expect(cli.argv).toHaveBeenCalled();
+  });
+
+  it("tracks argv spy number of calls", function() {
+    expect(cli.argv.calls.length).toEqual(1);
+  });
+
+  it("tracks all the arguments argv spy calls", function() {
+    expect(cli.argv).toHaveBeenCalledWith(that.argv);
+  });
+});
