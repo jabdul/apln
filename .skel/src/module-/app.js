16b8ed2b29aa6fb9722027146f62f350a49b14d0
James Abdul <james.abdul@craftturf.com>
2014-01-15 01:20:03 +0000

    Adding croosroads dependency to module configs.

--- .skel/src/module-/app.js
+++ .skel/src/module-/app.js
@@ -6,11 +6,13 @@ define([
   'core/connect',
   'log4javascript'],
 function (App, Doc, AppConfig, ModuleConfig, Connect, Log4j) {
+
+  // Setup configuration for the App object.
   App.setConfig({
     'projectName': ModuleConfig['module-{MODULE-NAME}'].projectName
   });
 
-  // Setup logger
+  // Setup logger for this module.
   // The getLogger string argument must be prefixed with Apl.Modules.
   ModuleConfig['module-{MODULE-NAME}'].Log =
     Log4j.getLogger( 'Apl.Modules.{MODULE-NAME-CAMEL}' );
@@ -19,11 +21,15 @@ function (App, Doc, AppConfig, ModuleConfig, Connect, Log4j) {
       'setEnabled': AppConfig.App.Logging.setEnabled
   });
 
+  // Initialise this module's config.
 	App.setModuleConfig({
 		'module-{MODULE-NAME}': ModuleConfig['module-{MODULE-NAME}']
 	});
 
+  // Setup default connection to server.
+  // This can also be dynamically changed during runtime.
 	var connect = new Connect(AppConfig.Server);
 	App.setConnection(connect);
+
 	return App;
 });
